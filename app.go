21c72e5fff4043ebcbd70d1979894f2be9ecc2d5
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-07-17 22:34:10 +0300

    finilize test for checking GET '/users'

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -13,7 +15,16 @@ type App struct {
 }
 
 // Initialize Init db connectio to SQL db
-func (a *App) Initialize(user, password, dbname string) {}
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("%s:%s@/%s", user, password, dbname)
+	var err error
+	a.DB, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+}
 
 // Run run our server by addr
 func (a *App) Run(addr string) {}
